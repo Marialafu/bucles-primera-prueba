596498e4e1abd961bb13d731e2f49e2064859cf9
Marialafu <lafuentelhmaria@gmail.com>
2025-03-25 10:53:35 +0100

    ultimos updates

--- js/scripts.js
+++ js/scripts.js
@@ -61,20 +61,20 @@ aleatoryNumber();
 
 console.log('Cuarto ejercicio NO SALE');
 
-const wordsList = ['Agua', 'Munición', 'Botiquín', 'Czst']
+const wordsList = ['Agua', 'Munición', 'Botiquín', 'Czst'];
 
 const identifyVowels = () => {
   const vowels = 'aeiou';
-  for (const word of wordsList){
-    for (const letter of word){
+  for (const word of wordsList) {
+    for (const letter of word) {
       if (vowels.includes(letter.toLowerCase())) {
-        console.log(`${word} contiene vocales`)
+        console.log(`${word} contiene vocales`);
       } else {
       }
     }
   }
-}
-identifyVowels()
+};
+identifyVowels();
 
 // 5️⃣ Bego está calculando precios con descuento. Tiene un array con precios, (por ejemplo [150, 300, 50]). Si el precio es mayor a 100, aplica un 20% de descuento, si no, un 10%. Usa un bucle para mostrar cada precio original, descuento aplicado y precio final.
 // Ejemplo salida:
@@ -82,15 +82,26 @@ identifyVowels()
 // Precio original: 300 - Descuento aplicado: 60 - Precio final: 240
 // Precio original: 50 - Descuento aplicado: 5 - Precio final: 45
 
-const aplicateDiscounts = (pricesList) => {
-  for (const price of pricesList){
+//MODO LARGO
+
+const aplicateDiscounts = pricesList => {
+  for (const price of pricesList) {
     if (price > 100) {
-      console.log(`Precio original: ${price} - Descuento aplicado: ${price*0.2} - Precio final: ${price - price*0.2}`)
-    } else {console.log(`Precio original: ${price} - Descuento aplicado: ${price*0.1} - Precio final: ${price - price*0.1}`);
-     }
+      console.log(
+        `Precio original: ${price} - Descuento aplicado: ${
+          price * 0.2
+        } - Precio final: ${price - price * 0.2}`
+      );
+    } else {
+      console.log(
+        `Precio original: ${price} - Descuento aplicado: ${
+          price * 0.1
+        } - Precio final: ${price - price * 0.1}`
+      );
+    }
   }
-}
-aplicateDiscounts([150, 300, 50])
+};
+aplicateDiscounts([150, 300, 50]);
 
 // 6️⃣ Bego está calculando precios con descuento. Tiene un array con precios, (por ejemplo [150, 300, 50]). Si el precio es mayor a 100, aplica un 20% de descuento, si no, un 10%. Usa un bucle para mostrar cada precio original, descuento aplicado y precio final.
 // Ejemplo salida:
@@ -100,40 +111,40 @@ aplicateDiscounts([150, 300, 50])
 
 console.log('Sexto ejercicio: Mismo que el 5');
 
-
 // 7️⃣ Sabrina quiere verificar cuántos números aleatorios generados entre 1 y 100 son múltiplos de 3. Haz una función que haga esta comprobación con 10 números.
 // Ejemplo salida:
 // Números generados: [12, 35, 75, 9, 27, 4, 6, 88, 15, 3]
 // Múltiplos de 3 encontrados: 7
 
 const validateIfNumberIsMultipleOf3 = () => {
-  const aleatoryNumberList = [3]
-  for (let i = 0; i < 10; i++){
-  aleatoryNumberList.push(Math.floor(Math.random()*(100-1)+1))
+  const aleatoryNumberList = [3];
+  for (let i = 0; i < 10; i++) {
+    aleatoryNumberList.push(Math.floor(Math.random() * (100 - 1) + 1));
   }
   console.log(`Números generados:${aleatoryNumberList}`);
-  let list = []
-  for (const number of aleatoryNumberList){
-  if (number % 3 === 0) {
-  list.push(number)
-  } else {}
+  let list = [];
+  for (const number of aleatoryNumberList) {
+    if (number % 3 === 0) {
+      list.push(number);
+    }
   }
   console.log(`Múltiplos de 3 encontrados:${list.length}`);
-}
-validateIfNumberIsMultipleOf3()
+};
+validateIfNumberIsMultipleOf3();
 
 // 8️⃣ Macarena tiene un array con tres nombres y debe crear un array nuevo con las iniciales de cada nombre en mayúsculas usando un bucle.
 // Ejemplo entrada: ['Pedro', 'Ana', 'Luis']
 // Ejemplo salida: ['P', 'A', 'L']
 
-const firstLetterName = (names) => {
-  const initialsNames = []
-  for (const name of names){
-    initialsNames.push(name.charAt(0))
+const firstLetterName = names => {
+  const initialsNames = [];
+  for (const name of names) {
+    const initials = name.charAt(0).toUpperCase();
+    initialsNames.push(initials);
   }
-  console.log(initialsNames); 
-}
-firstLetterName(['Pedro', 'Ana', 'Luis'])
+  console.log(initialsNames);
+};
+firstLetterName(['Pedro', 'ana', 'Luis']);
 
 // 9️⃣ Abby está revisando mensajes cifrados. Tiene un array con palabras (cantidad y palabras a tu elección) que deben invertirse y mostrarse en minúsculas. Usa un bucle para procesarlas todas. Investiga sobre las funciones split(), reverse() y join()
 // Ejemplo entrada: ['FUEGO', 'REFUGIO', 'SUMINISTROS']
@@ -143,44 +154,35 @@ firstLetterName(['Pedro', 'Ana', 'Luis'])
 //Reverse: invierte el orden de un array. array.reverse()
 //Join: une los elementos array en un string. array.join(que lo delimita)
 
-//NO SÉ HACERLO
-
-const descifrateWords = (words) => {
-
-  for (const divideWord of words){
-    console.log(divideWord.split(''))
+const descifrateWords = words => {
+  const reversedWords = [];
+  for (const word of words) {
+    const reverseLetters = word.split('').reverse().join('').toLowerCase();
+    reversedWords.push(reverseLetters);
   }
-  const dividedWord = []
-  console.log(dividedWord);
-  
-  //for (const unifyWord of divideWord){
-      //console.log(unifyWord);
-  //}
-
-    
-}
-descifrateWords(['FUEGO', 'REFUGIO', 'SUMINISTROS'])
-
+  console.log(reversedWords);
+};
+descifrateWords(['FUEGO', 'REFUGIO', 'SUMINISTROS']);
 
 // 🔟 Camila quiere generar un código de acceso combinando letras aleatorias y números. Usa un bucle para crear un string aleatorio de 6 caracteres sacados de 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.
 // Ejemplo salida: 'A3P9K8'
 
-const aleatoryPosition = () => {
-  const aleatoryCharacter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-  const aleatoryNumber = Math.floor(Math.random()*aleatoryCharacter.length)
+const aleatoryCharacter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-  return aleatoryNumber
-}
+const aleatoryPosition = () => {
+  const aleatoryNumber = Math.floor(Math.random() * aleatoryCharacter.length);
+  return aleatoryNumber;
+};
 
 const aleatoryAccesCode = () => {
-  const aleatoryCharacter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-  const codeList = []
-  for (let i = 0; i < 6; i++){
-    console.log(codeList.push(aleatoryCharacter.charAt(aleatoryPosition())))
+  let code = '';
+  for (let i = 0; i < 6; i++) {
+    code = aleatoryCharacter.charAt(aleatoryPosition());
   }
-  console.log(codeList)
-}
-aleatoryAccesCode()
+
+  console.log(code);
+};
+aleatoryAccesCode();
 
 // 1️⃣1️⃣ Macarena quiere crear un sistema que genere combinaciones de nombres y apellidos. Tiene dos arrays: ['Juan', 'Ana', 'Luis'] y ['Pérez', 'Martínez', 'García']. Debe generar todos los nombres posibles combinando uno de cada array.
 // Ejemplo salida:
@@ -195,16 +197,16 @@ aleatoryAccesCode()
 // Luis García
 
 const namesAndSurnamesGenerator = () => {
-  const names = ['Juan', 'Ana', 'Luis']
-  const surnames = ['Pérez', 'Martínez', 'García']
+  const names = ['Juan', 'Ana', 'Luis'];
+  const surnames = ['Pérez', 'Martínez', 'García'];
 
-  for (const name of names){
-    for (const surname of surnames){
+  for (const name of names) {
+    for (const surname of surnames) {
       console.log(`${name} ${surname}`);
     }
   }
-}
-namesAndSurnamesGenerator()
+};
+namesAndSurnamesGenerator();
 
 // 1️⃣2️⃣ Abby quiere comprobar si al menos uno de los números generados aleatoriamente entre 1 y 100 es divisible por 5. Genera 5 números y muestra si se cumple la condición.
 // Ejemplo salida:
@@ -214,40 +216,37 @@ namesAndSurnamesGenerator()
 //Está mal, no sñe que se mete en el push.
 
 const aleatoryNumberGenerator = () => {
-  const aleatoryNumber = Math.floor(Math.random()*(100-1)+1)
+  const aleatoryNumber = Math.floor(Math.random() * (100 - 1) + 1);
 
-  return aleatoryNumber
-}
+  return aleatoryNumber;
+};
 
 const validateIfNumberCanBeDividedBy5 = () => {
-  const numberList = []
-  const numbersDividedBy5 = []
-  for (let i = 0; i < 5; i++){
-  numberList.push(aleatoryNumberGenerator())
-  if (i % 5 === 0){
-   numbersDividedBy5.push();
-  } else {'ninguno es divisible';}
-}
+  const numberList = [];
+  const numbersDividedBy5 = [];
+  for (let i = 0; i < 5; i++) {
+    numberList.push(aleatoryNumberGenerator());
+    if (i % 5 === 0) {
+      numbersDividedBy5.push();
+    }
+  }
   console.log(`Números generados: ${numberList}`);
   console.log(`Números divisibles por 5 encontrados: ${numbersDividedBy5}`);
-
-}
-validateIfNumberCanBeDividedBy5()
+};
+validateIfNumberCanBeDividedBy5();
 
 // 1️⃣3️⃣ Camila tiene un array con nombres y quiere mostrar sólo aquellos que tienen más de 4 letras.
 // Ejemplo entrada: ['Pedro', 'Ana', 'Luis', 'Elena', 'Sofía']
 // Ejemplo salida: ['Pedro', 'Elena', 'Sofía']
 
-const determineLengthNames = (names) => {
-  const fourLetterNames = []
-  for (const name of names){
-    name.length > 4 ? fourLetterNames.push(name) : 'nada'
+const determineLengthNames = names => {
+  const fourLetterNames = [];
+  for (const name of names) {
+    name.length > 4 ? fourLetterNames.push(name) : 'nada';
   }
   console.log(fourLetterNames);
-  
-}
-determineLengthNames(['Pedro', 'Ana', 'Luis', 'Elena', 'Sofía'])
-
+};
+determineLengthNames(['Pedro', 'Ana', 'Luis', 'Elena', 'Sofía']);
 
 // 1️⃣4️⃣ Bego tiene un array con tres palabras y quiere mostrar todas las combinaciones posibles de esas palabras usando dos palabras a la vez y cuyas combinaciones no repitan la palabra.
 // Ejemplo entrada: ['Sol', 'Luna', 'Estrella']
@@ -260,44 +259,43 @@ determineLengthNames(['Pedro', 'Ana', 'Luis', 'Elena', 'Sofía'])
 // Estrella Luna
 
 const wordCombiner = () => {
-  const firstList = ['Sol', 'Luna', 'Estrella']
-  const secondList = ['Sol', 'Luna', 'Estrella']
-  for (const firstWord of firstList){
-    for (const secondWord of secondList){
+  const firstList = ['Sol', 'Luna', 'Estrella'];
+  const secondList = ['Sol', 'Luna', 'Estrella'];
+  for (const firstWord of firstList) {
+    for (const secondWord of secondList) {
       console.log(firstWord + ' ' + secondWord);
-      
     }
-    
   }
-}
-wordCombiner()
+};
+wordCombiner();
 
 // 1️⃣5️⃣ Sabrina quiere generar un nombre de usuario aleatorio combinando una consonante, una vocal y un número aleatorio entre 1 y 99. Debe hacerlo 5 veces y mostrar los resultados.
 // Ejemplo salida: ['MA87', 'RO56', 'LE23', 'FI99', 'PU12']
 
-
 const userNameGenerator = () => {
-  const vowels = 'aeiou'
-  const aleatoryVowel = Math.floor(Math.random()*vowels.length)
-  const consonant = 'bcdfghjklmnñpqrstvwxyz'
-  const aleatoryConsonant = Math.floor(Math.random()*consonant.length)
-  const aleatoryNumber = Math.floor(Math.random()*99)
+  const vowels = 'aeiou';
+  const aleatoryVowel = Math.floor(Math.random() * vowels.length);
+  const consonant = 'bcdfghjklmnñpqrstvwxyz';
+  const aleatoryConsonant = Math.floor(Math.random() * consonant.length);
+  const aleatoryNumber = Math.floor(Math.random() * 99);
 
-  const userNameGenerator = consonant.charAt(aleatoryConsonant).toUpperCase() + vowels.charAt(aleatoryVowel).toUpperCase() + aleatoryNumber
+  const userNameGenerator =
+    consonant.charAt(aleatoryConsonant).toUpperCase() +
+    vowels.charAt(aleatoryVowel).toUpperCase() +
+    aleatoryNumber;
 
-  return userNameGenerator
-}
-userNameGenerator()
+  return userNameGenerator;
+};
+userNameGenerator();
 
 const userNameList = () => {
-  const userNameList = []
-  for (let i = 0; i < 6; i++){
-    userNameList.push(userNameGenerator())
+  const userNameList = [];
+  for (let i = 0; i < 6; i++) {
+    userNameList.push(userNameGenerator());
   }
   console.log(userNameList);
-}
-userNameList()
-
+};
+userNameList();
 
 // 1️⃣6️⃣ Abby tiene problemas con las tablas de multiplicar, quiere hacer 2 funciones que reciban un número y le impriman la tabla de multiplicar de ese número desde hasta el 10 una en orden descendente y otra en orden ascendente.
 
@@ -331,34 +329,64 @@ userNameList()
 
 //SIN REVERSE
 
-const multiplicationTableUpward = (number) => {
-  const concadenateNumbersTable = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+const multiplicationTableUpward = number => {
+  const concadenateNumbersTable = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
+  ];
 
   console.log('Función ascendente');
-  for (const concadenateNumbers of concadenateNumbersTable){
-    console.log(`${number} x ${concadenateNumbers} = ${number*concadenateNumbers}`);
+  for (const concadenateNumbers of concadenateNumbersTable) {
+    console.log(
+      `${number} x ${concadenateNumbers} = ${number * concadenateNumbers}`
+    );
   }
 
   console.log('Función descendente');
-  for (let i = concadenateNumbersTable.length - 1; i >= 0; i--){
-    console.log(`${number} x ${i} = ${number*i}`);
+  for (let i = concadenateNumbersTable.length - 1; i >= 0; i--) {
+    console.log(`${number} x ${i} = ${number * i}`);
   }
-}
+};
 //multiplicationTableUpward(4)
 
 //CON REVERSE
 
-const multiplicationTableUpward1 = (number) => {
-  const concadenateNumbersTable = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+const multiplicationTableUpward1 = number => {
+  const concadenateNumbersTable = [
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
+  ];
 
   console.log('Función ascendente');
-  for (const concadenateNumbers of concadenateNumbersTable){
-    console.log(`${number} x ${concadenateNumbers} = ${number*concadenateNumbers}`);
+  for (const concadenateNumbers of concadenateNumbersTable) {
+    console.log(
+      `${number} x ${concadenateNumbers} = ${number * concadenateNumbers}`
+    );
   }
 
   console.log('Función descendente');
-  for (const concadenateNumbers of concadenateNumbersTable.reverse()){
-    console.log(`${number} x ${concadenateNumbers} = ${number*concadenateNumbers}`);
+  for (const concadenateNumbers of concadenateNumbersTable.reverse()) {
+    console.log(
+      `${number} x ${concadenateNumbers} = ${number * concadenateNumbers}`
+    );
   }
-}
+};
 //multiplicationTableUpward1(3)
